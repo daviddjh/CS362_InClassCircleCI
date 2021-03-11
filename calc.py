61b81ae902ca763a5ed65a8759a44aaaf9e7c724
David Headrick <david.headrick9@gmail.com>
2021-03-11 08:20:51 -0800

    First commit

---
+++ calc.py
@@ -0,0 +1,11 @@
+def add(a, b):
+  if (type(a) == int or type(a) == float) and (type(b) == int or type(b) == float):
+    return a + b 
+  else:
+    raise TypeError("Only Numbers Allowed")""
+
+def multiply(a, b):
+  if (type(a) == int or type(a) == float) and (type(b) == int or type(b) == float):
+    return a * b 
+  else:
+    raise TypeError("Only Numbers Allowed")""
