908a835c81098ec85f6fc4115bc6ed36b768f73f
David Headrick <david.headrick9@gmail.com>
2021-03-11 09:08:22 -0800

    Fix whitespace in test

--- test_calculator.py
+++ test_calculator.py
@@ -9,9 +9,9 @@ class TestCalculator:
 
     def test_add(self):
         assert 5 == calc.add(1, 4)
-            
+
     def test_subtract(self):
         assert 2 == calc.subtract(5, 3)
-                        
+
     def test_multiply(self):
         assert 2 == calc.multiply(1, 2)
